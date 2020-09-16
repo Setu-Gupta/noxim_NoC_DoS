3027caaba7e448d7b6a45891f4fc078fd66090b7
Setu-Gupta <setu18190@iiitd.ac.in>
2020-09-16 18:33:45 +0530

    Added some new benchmarks for transpose and added a new plotter file
    On branch cyclefix
    Changes to be committed:
    	modified:   ../../.gitignore
    	modified:   ../test_vectors/Barnes_0.00001_attack
    	modified:   ../test_vectors/FFT_attack
    	modified:   ../test_vectors/Radix_0.00001_attack
    	modified:   ../test_vectors/backprop_traces_64_Network_File.txt_attack
    	modified:   ../test_vectors/gaussian_traces_64_Network_File.txt_attack
    	modified:   ../test_vectors/hotspot_traces_64_Network_File.txt_attack
    	modified:   ../test_vectors/kmeans_traces_64_Network_File.txt_attack
    	modified:   ../test_vectors/lud_traces_64_Network_File.txt_attack
    	modified:   ../test_vectors/mummergpu_traces_64_Network_File.txt_attack
    	modified:   .gitignore
    	new file:   plot_gen/router_accuracy/plotter_line.py

---
+++ bin/tools/plot_gen/router_accuracy/plotter_line.py
@@ -0,0 +1,58 @@
+'''
+Usage: python3 path/to/this/file path/to/accuracy/report
+Makes a 3dbar graph for accuracy
+'''
+
+from mpl_toolkits import mplot3d
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import matplotlib.colors as colors
+import sys
+
+def get_accuracy():
+	accuracy = {}
+	with open(sys.argv[1], 'r') as report:
+		for line in report:
+			data = line.split()
+			data.remove(":")
+
+			router = data[0].split("_")
+			key_x = int(router[0])
+			key_y = int(router[1])
+			key_id = key_x + 8*key_y + 1
+			key_dir = 1 if 'in' in router[2] else 0
+			key = (key_id, key_dir)
+
+			acc = float(data[1].replace(",", ""))
+
+			accuracy[key] = acc
+	return accuracy
+
+def f(accuracy, in_out):
+	x = []
+	y = []
+	for x_val in range(1,65):
+		x.append(x_val)
+		y.append(accuracy[(x_val, in_out)])
+	return x, y
+
+
+accuracy = get_accuracy()
+
+x, y = f(accuracy, 1)
+
+cmap = cm.get_cmap('viridis') # Get desired colormap - you can change this!
+max_height = np.max(y)   # get range of colorbars so we can normalize
+min_height = np.min(y)
+rgba = [cmap((k-min_height)/(max_height-min_height)) for k in y]
+
+# plt.bar(x, y, color=rgba)		# <----------- Uncomment this for bar graph
+plt.plot(x,y,'r')		# <----------- Uncomment this for line graph
+plt.title(label="Accuracy for different routers", fontsize=18)
+plt.xlabel("Router ID", fontsize=16)
+plt.xticks(list(range(2, 65, 2)))
+plt.ylabel("Accuracy in %", fontsize=16)
+plt.autoscale(enable=True, axis='x', tight=True)
+# plt.grid()	# <--------- Uncomment this for grid
+plt.show()
